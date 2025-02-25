6429b7b77a4efabe99e68fe3889805b73a7f8e62
SegnonDimas <segnondimas@gmail.com>
2025-02-25 18:13:12 +0100

    ElevatedButton

--- lib/pages/basicsWidgets_learn.dart
+++ lib/pages/basicsWidgets_learn.dart
@@ -9,24 +9,20 @@ class BasicsWidgetsLearn extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue.shade800,
         foregroundColor: Colors.white,
-        title: const Text('C  O  N  T  A  I  N  E  R'),
+        title: const Text('E L E V A T E D B U T T O N'),
         centerTitle: true,
       ),
       body: Center(
-          child: Badge(
-        backgroundColor: Colors.green,
-        //label: Text('20'),
-        smallSize: 40,
-        //largeSize: 40,
-        alignment: Alignment.bottomRight,
-        child: Container(
-          height: 100,
-          width: 150,
-          decoration: BoxDecoration(
-              color: Colors.blue.shade800,
-              borderRadius: BorderRadius.circular(30)),
-        ),
-      )),
+          child: ElevatedButton(
+              onPressed: () {}, child: const Icon(Icons.touch_app_outlined))),
     );
   }
 }
+
+/*
+ floatingActionButton: Text(
+        "$alignment",
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+*/
